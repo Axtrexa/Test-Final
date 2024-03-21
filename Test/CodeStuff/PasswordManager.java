83807572e7bfec9a249d8d68fc7a61c3db4681f0
ttien14 <163856663+ttien14@users.noreply.github.com>
2024-03-21 08:20:18 -0400

    Changed i = to int index =

--- Test/CodeStuff/PasswordManager.java
+++ Test/CodeStuff/PasswordManager.java
@@ -1,23 +1,19 @@
 package Test.CodeStuff;
 import java.util.Random;
 public class PasswordManager {
-    private String password;
     
     public static String passwordGenerator(int length){
         String characters = "0123456789";
         StringBuilder randomPass = new StringBuilder();
         Random gen = new Random();
         for(int i = 0; i < length; i++){
-             i = gen.nextInt(characters.length());
-            randomPass.append(characters.charAt(i));
+             int index = gen.nextInt(characters.length());
+            randomPass.append(characters.charAt(index));
 
         }
         return randomPass.toString();
     }
     
-    
-
-
     public boolean verifyPassword(String password)
     {
         if(this.password == password)
