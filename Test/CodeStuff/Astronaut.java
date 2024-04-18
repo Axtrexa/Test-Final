889601b8e212d5264e9d3bb4377e9254909e6019
ttien14 <163856663+ttien14@users.noreply.github.com>
2024-04-18 08:25:57 -0400

    Add files via upload

---
+++ Test/CodeStuff/Astronaut.java
@@ -0,0 +1,97 @@
+import java.util.Scanner;
+
+public class Astronaut {
+    public String AstroName;
+    public String Birthdate;
+    public String SerialNum;
+    public String Address;
+    public String Email;
+    public String PhoneNum;
+    public double PayRate;
+    public double AstroWeight;
+    public String NexttoKin;
+    public String AstroStatus;
+
+    // Get Astronaut's name
+    public String getAstroName(){
+        return AstroName;
+    }
+    // Get Astronaut's birthdate
+    public String getBirthdate(){
+        return Birthdate;
+    }
+    // Get Astronaut's generated serial
+    public String getSerialNum(){
+        return SerialNum;
+    }
+    // Get Astronaut's address
+    public String getAddress(){
+        return Address;
+    }
+    // Get Astronaut's email
+    public String getEmail(){
+        return Email;
+    }
+    // Get Astronaut's phone number
+    public String getPhoneNum(){
+        return PhoneNum;
+    }
+    // Get Astronaut's pay rate
+    public double getPayRate(){
+        return PayRate;
+    }
+    // Get Astronaut's weight
+    public double getAstroWeigh(){
+        return AstroWeight;
+    }
+    // Get Astronaut's next to kin
+    public String getNexttoKin(){
+        return NexttoKin;
+    }
+    // Get Astronaut's marital status
+    public String getAstroStatus(){
+        return AstroStatus;
+    }
+    // Set Astronaut's name
+    public void setAstroName(String AstroName){
+        this.AstroName = AstroName;
+    }
+    // Set Astronaut's age
+    public void setBirthdate(String Birthdate){
+        this.Birthdate = Birthdate;
+    }
+    // Set Astronaut's generated serial
+    public void setSerialNum(String SerialNum){
+        this.SerialNum = SerialNum;
+    }
+    // Set Astronaut's address
+    public void setAddress(String Address){
+        this.Address = Address;
+    }
+    // Set Astronaut's email
+    public void setEmail(String Email){
+        this.Email = Email;
+    }
+    // Set Astronaut's phone number
+    public void setPhoneNum(String PhoneNum){
+        this.PhoneNum = PhoneNum;
+    }
+    // Set Astronaut's pay rate
+    public void setPayRate(double PayRate){
+        this.PayRate = PayRate;
+    }
+    //Set Astronaut's weight
+    public void setAstroWeight(double AstroWeight){
+        this.AstroWeight = PayRate;
+    }
+    // Set Astronaut's next to kin
+    public void setNexttoKin(String NexttoKin){
+        this.NexttoKin = NexttoKin;
+    }
+    // Set Astronaut's marital status
+    public void setAstroStatus(String AstroStatus){
+        this.AstroStatus = AstroStatus;
+    }
+    }
+    
+}
