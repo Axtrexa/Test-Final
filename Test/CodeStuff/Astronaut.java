879ab5c8bb8fb4ec19bf99971cda934d5233d7b0
ttien14 <ttien3056@gmail.com>
2024-04-29 08:24:51 -0400

    Donezo but needs help

---
+++ Test/CodeStuff/Astronaut.java
@@ -0,0 +1,254 @@
+package Test.CodeStuff;
+public class Astronaut {
+    private String AstroName;
+    public String Birthdate;
+    private String SerialNum;
+    private String Address;
+    private String Email;
+    private String PhoneNum;
+    private String PayRate;
+    private String AstroWeight;
+    private String NexttoKin;
+    private String AstroStatus;
+
+    // Get Astronaut's name
+    public String getAstroName(){
+        return AstroName;
+    }
+    // Get Astronaut's birthdate
+    public String getBirthdate(){
+        return Birthdate;
+    }
+    // Get Astronaut's generated serial
+    public String getSerialNum(){
+        return SerialNum;
+    }
+    // Get Astronaut's address
+    public String getAddress(){
+        return Address;
+    }
+    // Get Astronaut's email
+    public String getEmail(){
+        return Email;
+    }
+    // Get Astronaut's phone number
+    public String getPhoneNum(){
+        return PhoneNum;
+    }
+    // Get Astronaut's pay rate
+    public String getPayRate(){
+        return PayRate;
+    }
+    // Get Astronaut's weight
+    public String getAstroWeight(){
+        return AstroWeight;
+    }
+    // Get Astronaut's next to kin
+    public String getNexttoKin(){
+        return NexttoKin;
+    }
+    // Get Astronaut's status
+    public String getAstroStatus(){
+        return AstroStatus;
+    }
+    // Set Astronaut's name
+    public void setAstroName(String AstroName){
+        this.AstroName = AstroName;
+    }
+    // Set Astronaut's age
+    public void setBirthdate(String Birthdate){
+        this.Birthdate = Birthdate;
+    }
+    // Set Astronaut's generated serial
+    public void setSerialNum(String SerialNum){
+        this.SerialNum = SerialNum;
+    }
+    // Set Astronaut's address
+    public void setAddress(String Address){
+        this.Address = Address;
+    }
+    // Set Astronaut's email
+    public void setEmail(String Email){
+        this.Email = Email;
+    }
+    // Set Astronaut's phone number
+    public void setPhoneNum(String PhoneNum){
+        this.PhoneNum = PhoneNum;
+    }
+    // Set Astronaut's pay rate
+    public void setPayRate(String PayRate){
+        this.PayRate = PayRate;
+    }
+    //Set Astronaut's weight
+    public void setAstroWeight(String AstroWeight){
+        this.AstroWeight = AstroWeight;
+    }
+    // Set Astronaut's next to kin
+    public void setNexttoKin(String NexttoKin){
+        this.NexttoKin = NexttoKin;
+    }
+    // Set Astronaut's status
+    public void setAstroStatus(String AstroStatus){
+        this.AstroStatus = AstroStatus;
+    }
+
+    public static String serialNumGenerator(int length){
+        String characters = "0123456789";
+        StringBuilder randomSerial = new StringBuilder();
+        Random gen = new Random();
+        for(int i = 0; i < length; i++){
+             int index = gen.nextInt(characters.length());
+            randomSerial.append(characters.charAt(index));
+
+        }
+        return randomSerial.toString();
+    }
+
+    public static boolean NameValidation(String AstroName){
+        for(char name: AstroName.toCharArray()){
+            if(!Character.isLetter(name) && name != ' '){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean BirthdateValidation(String Birthdate){
+
+        int day;
+        int month;
+        int year;
+
+        if(Birthdate.length() != 10){
+            return false;
+        }
+
+        try{
+        day = Integer.parseInt(Birthdate.substring(0, 2));
+        month = Integer.parseInt(Birthdate.substring(3,5));
+        year = Integer.parseInt(Birthdate.substring(6));
+        } catch (NumberFormatException e){
+            return false;
+        }
+
+        boolean isValid = true;
+        if(day < 1 || day > 31){
+            isValid = false;
+        }
+        if(month < 01 || month > 12){
+            isValid = false;
+        }
+        if(year < 1900 || year > 2024){
+            isValid = false;
+        }
+        for(int i = 0; i < Birthdate.length(); i++){
+            switch(i){
+                case 2:
+                case 5:
+                    if(Birthdate.charAt(i) != '/'){
+                        return false;
+                    }
+                    break;
+                default:
+                    if(!Character.isDigit(Birthdate.charAt(i))){
+                        return false;
+                    }
+            }
+        }
+        return isValid;
+    }
+
+    public static boolean AddressValidation(String Address){
+        for(int i = 0; i < Address.length(); i++){
+            char address = Address.charAt(i);
+            if(!Character.isLetter(address) && !Character.isDigit(address) && address != ' ' && address != '/'){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean EmailValidation(String Email){
+        int atCount = 0;
+        int dotCount = 0;
+
+        if(Email.length() <= 10){
+            return false;
+        }
+
+        for(int i = 0; i < Email.length(); i ++){
+            char email = Email.charAt(i);
+            if(email == '@'){
+                atCount ++;
+            }
+            else if (email == '.'){
+                dotCount ++;
+            }
+            else if (!Character.isLetter(email) && !Character.isDigit(email) && email != '_' && email != '-'){
+                return false;
+            }
+        }
+        if(atCount != 1 || dotCount < 1){
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean PhoneNumValidation(String PhoneNum){
+        if(PhoneNum.length() != 13){
+            return false;
+        }
+        for(int i = 0; i < PhoneNum.length(); i++){
+            switch(i){
+                case 0:
+                    if(PhoneNum.charAt(i) != '('){
+                        return false;
+                    }
+                    break;
+                case 4:
+                    if(PhoneNum.charAt(i) != ')'){
+                        return false;
+                    }
+                    break;
+                case 8:
+                    if(PhoneNum.charAt(i) != '-'){
+                        return false;
+                    }
+                    break;
+                default:
+                    if(!Character.isDigit(PhoneNum.charAt(i))){
+                        return false;
+                    }
+            }
+        }
+        return true;
+    }
+
+    public static boolean PayRateValidation(String PayRate){
+        
+    }
+
+    public static boolean WeightValidation(String AstroWeight){
+    }
+
+    public static boolean NexttoKinValidation(String NexttoKin){
+        for(char nextKin: NexttoKin.toCharArray()){
+            if(!Character.isLetter(nextKin)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean AstroStatusValidation(String AstroStatus){
+        for(char astroStat: AstroStatus.toCharArray()){
+            if(!Character.isLetter(astroStat)){
+                return false;
+            }
+        }
+        return true;
+        }
+}
+
+
+    
---
+++ Test/CodeStuff/Spacecraft.java
@@ -0,0 +1,181 @@
+package Test.CodeStuff;
+import java.util.Scanner;
+public class Spacecraft {
+    public static void main(String[] args){
+        int selection;
+
+        Scanner keyboard = new Scanner(System.in);
+        Astronaut AstroInfo = new Astronaut();
+        System.out.println("Welcome aboard Astronaut!");
+        System.out.println("ASTRONAUT INFO:");
+        displayAstroInfo(AstroInfo);
+        System.out.println("-----------------------------");
+
+
+        do{
+        InfoMenu();
+        System.out.println("Please enter your choice from the following options:");
+        while(!keyboard.hasNextInt()){
+            System.out.println("Invalid selection. Please try again.");
+            keyboard.nextLine();
+        }
+        selection = keyboard.nextInt();
+        keyboard.nextLine();
+
+            if(selection < 1 || selection > 10){
+                System.out.println("Invalid selection. Please try again.");
+                System.out.println("-----------------------------");
+            }
+            else{
+            switch(selection){
+                case 1:
+                    System.out.println("Please enter your first and last name:");
+                    String AstroName = keyboard.nextLine();
+                    while(!Astronaut.NameValidation(AstroName)){
+                        System.out.println("Invalid mame format. Please enter letters and spaces only.");
+                        AstroName = keyboard.nextLine();
+                    }
+                    AstroInfo.setAstroName(AstroName);
+                    System.out.println("Astronaut's information has been changed!");
+                    System.out.println("-----------------------------");
+                    displayAstroInfo(AstroInfo);
+                    break;
+                case 2:
+                    System.out.println("Please enter your birthdate (dd/mm/yyyy):");
+                    String Birthdate = keyboard.nextLine();
+                    while(!Astronaut.BirthdateValidation(Birthdate)){
+                        System.out.println("This date is invalid. Please try again in the following format (dd/mm/yyyy):");
+                        Birthdate = keyboard.nextLine();
+                    }
+                    AstroInfo.setBirthdate(Birthdate);
+                    System.out.println("Astronaut's information has been changed!");
+                    System.out.println("-----------------------------");
+                    displayAstroInfo(AstroInfo);
+                    break;
+                case 3:
+                    System.out.println("Please enter your address:");
+                    String Address = keyboard.nextLine();
+                    while(!Astronaut.AddressValidation(Address)){
+                        System.out.println("This address is invalid. Please try again.");
+                        Address = keyboard.nextLine();
+                    }
+                    AstroInfo.setAddress(Address);
+                    System.out.println("Astronaut's information has been changed!");
+                    System.out.println("-----------------------------");
+                    displayAstroInfo(AstroInfo);
+                    break;
+                case 4:
+                    System.out.println("Please enter your email:");
+                    String Email = keyboard.nextLine();
+                    while(!Astronaut.EmailValidation(Email)){
+                        System.out.println("This email is invalid. Please try again in the following format (name@example.com):");
+                        Email = keyboard.nextLine();
+                    }
+                    AstroInfo.setEmail(Email);
+                    System.out.println("Astronaut's information has been changed!");
+                    System.out.println("-----------------------------");
+                    displayAstroInfo(AstroInfo);
+                    break;
+                case 5:
+                    System.out.println("Please enter your phone number in the following format ((XXX)XXX-XXXX):");
+                    String PhoneNum = keyboard.nextLine();
+                    while(!Astronaut.PhoneNumValidation(PhoneNum)){
+                        System.out.println("This number is invalid. Please try again in the following format ((XXX)XXX-XXXX):");
+                        PhoneNum = keyboard.nextLine();
+                    }
+                    AstroInfo.setPhoneNum(PhoneNum);
+                    System.out.println("Astronaut's information has been changed!");
+                    System.out.println("-----------------------------");
+                    displayAstroInfo(AstroInfo);
+                    break;
+                case 6:
+                    System.out.println("Please enter your pay rate:");
+                    String PayRate = keyboard.nextLine();
+                    while(!Astronaut.PayRateValidation(PayRate)){
+                        System.out.println("This is an invalid pay rate. Please try again in the following format ($X,XXX,XXX):");
+                        PayRate = keyboard.nextLine();
+                    }
+                    AstroInfo.setPayRate(PayRate);
+                    System.out.println("Astronaut's information has been changed!");
+                    System.out.println("-----------------------------");
+                    displayAstroInfo(AstroInfo);
+                    break;
+                case 7:
+                    System.out.println("Please enter your weight in pounds:");
+                    String AstroWeight = keyboard.nextLine();
+                    AstroInfo.setAstroWeight(AstroWeight);
+                    System.out.println("Astronaut's information has been changed!");
+                    System.out.println("-----------------------------");
+                    displayAstroInfo(AstroInfo);
+                    break;
+                case 8:
+                    System.out.println("Please enter your next of kin:");
+                    String NexttoKin = keyboard.nextLine();
+                    while(!Astronaut.NexttoKinValidation(NexttoKin)){
+                        System.out.println("Invalid format. Please enter letters only.");
+                        NexttoKin = keyboard.nextLine();
+                    }
+                    AstroInfo.setNexttoKin(NexttoKin);
+                    System.out.println("Astronaut's information has been changed!");
+                    System.out.println("-----------------------------");
+                    displayAstroInfo(AstroInfo);
+                    break;
+                case 9:
+                    System.out.println("Please enter your status:");
+                    String AstroStatus = keyboard.nextLine();
+                    while(!Astronaut.AstroStatusValidation(AstroStatus)){
+                        System.out.println("Invalid format. Please enter letters only.");
+                        AstroStatus = keyboard.nextLine();
+                    }
+                    AstroInfo.setAstroStatus(AstroStatus);
+                    System.out.println("Astronaut's information has been changed!");
+                    System.out.println("-----------------------------");
+                    displayAstroInfo(AstroInfo);
+                    break;
+                case 10:
+                do{
+                    displayAstroInfo(AstroInfo);
+                    System.out.println("Type 'Y' to confirm or 'N' to go back:");
+                    char confirmChoice = keyboard.nextLine().charAt(0);
+
+                    if(confirmChoice == 'y' || confirmChoice == 'Y'){
+                        System.out.println("Information has been confirmed. Proceeding to the next step");
+                        break;
+                    } else if (confirmChoice == 'n' || confirmChoice == 'N') {
+                        displayAstroInfo(AstroInfo);
+                        break;
+                    } else{
+                        System.out.println("This option is invalid. Please type 'Y' or 'N' to confirm or deny.");
+                    }
+                } while(true);
+                break;
+            }
+        }
+    }   while(true);
+}
+
+    public static void InfoMenu(){
+        System.out.println("1. Edit Astronaut's Name");
+        System.out.println("2. Edit Astronaut's Birthdate");
+        System.out.println("3. Edit Astronaut's Address");
+        System.out.println("4. Edit Astronaut's Email");
+        System.out.println("5. Edit Astronaut's Phone Number");
+        System.out.println("6. Edit Astronaut's Pay Rate");
+        System.out.println("7. Edit Astronaut's Weight");
+        System.out.println("8. Edit Astronaut's Next of Kin");
+        System.out.println("9. Edit Astronaut's Status");
+        System.out.println("10. Confirm information is correct?");
+    }
+
+    public static void displayAstroInfo(Astronaut AstroInfo){
+            System.out.println("Name: " + AstroInfo.getAstroName());
+            System.out.println("Birthdate: " + AstroInfo.getBirthdate());
+            System.out.println("Address: " + AstroInfo.getAddress());
+            System.out.println("Email: " + AstroInfo.getEmail());
+            System.out.println("Phone Number: " + AstroInfo.getPhoneNum());
+            System.out.println("Pay Rate: " + AstroInfo.getPayRate());
+            System.out.println("Weight: " + AstroInfo.getAstroWeight());
+            System.out.println("Next of Kin: " + AstroInfo.getNexttoKin());
+            System.out.println("Status: " + AstroInfo.getAstroStatus());
+        }
+}
